8dc1a5924cc23ee7de5a4db96af20668b1ecb355
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-08-11 12:06:45 -0400

    get route to get specific quizzes from database

--- client/src/components/earnCoins/quizList.jsx
+++ client/src/components/earnCoins/quizList.jsx
@@ -8,31 +8,43 @@ import StatsCard from "../StatsCard.jsx"
 export default class QuizList extends Component{
   constructor(props){
     super(props)
+    this.openQuiz = this.openQuiz.bind(this);
     this.state = {
       quizzes: [],
     }
   }
+
+
+  openQuiz(e){
+    alert("Hello")
+    axios.get("http://localhost:5000/quizzes/" + e.target.id)
+    .then(quiz => {console.log(quiz.data)})
+    .catch(err => {console.log(err)})
+  }
   componentDidMount(){
     axios.get("https://next-world.herokuapp.com/quizzes/")
-    .then(question => {
-      console.log(question.data)
+    .then(quizzes => {
+    //  console.log(quizzes.data)
       this.setState({
-        quizzes: question.data
+        quizzes: quizzes.data
       })//end of this.setSTate
-      this.state.quizzes.map(quiz => {console.log(quiz)})
     })//end of .then statement
     .catch(error => {
       console.log(error)
     })//end of catch block
-
-
   }//componentDidMount
+
   quizNames(){
     return this.state.quizzes.map(quiz=>{
       return(
         <div className = "text-center">
-        <a href = {"/quiz-list/" + quiz._id}> {quiz.QuizName}</a>
+        <form id = {quiz._id} onSubmit = {this.openQuiz}>
+        <button type= "submit"> {quiz.QuizName}</button>
+        </form>
+        
+        <a href = {"/quiz-list/" + quiz._id}>
         <h1>{quiz.QuizName}</h1>
+          </a>
         </div>
       )
     })
@@ -43,6 +55,8 @@ render(){
     <div>
     <Header/>
     {this.quizNames()}
+
+    <button onClick={(this.openQuiz)}>Hello</button>
     <Footer/>
     </div>
   )//end of return
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -2,9 +2,15 @@ const router = require ("express").Router()
 const Quiz = require ("../models/quizzes.model")
 
 
-router.route("/").get((req,res)=>{
+router.route("/").get((req,res)=>{// get route for finding all the quizzes objects inside the quiz collection in the database
   Quiz.find()
   .then(item => res.json(item))
   .catch(err => res.status(400).json("Error " + err))//catches any errors and sends the error if there are any.
 })
+
+router.route("/:id").get((req,res) => {
+  Quiz.findOne({_id: req.params.id})
+  .then(item => res.json(item))
+  .catch(err => res.status(400).json("Error" + err))
+})//get route for finding a pecific quiz object in a collection given its id
 module.exports = router;//exporst the router functionality which will be used in an app.use funciton in the server.js file to render and use. The server.js can then use the .get and .post functionality of the router which was defined in this file
